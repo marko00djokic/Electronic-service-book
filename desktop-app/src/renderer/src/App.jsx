7dce25a20af18aa8de5336eaa2717cec4be4a9d7
Marko Đokić <96503127+kameleon1808@users.noreply.github.com>
2026-05-04 15:45:37 +0200

    feat: add vehicle and owner routes to React Router

--- desktop-app/src/renderer/src/App.jsx
+++ desktop-app/src/renderer/src/App.jsx
@@ -1,25 +1,38 @@
-import { HashRouter, Routes, Route } from 'react-router-dom'
-import Layout from './components/layout/Layout'
-import Dashboard from './pages/Dashboard'
-import Vehicles from './pages/Vehicles'
-import Owners from './pages/Owners'
-import ServiceOrders from './pages/ServiceOrders'
-import Catalog from './pages/Catalog'
-import Reminders from './pages/Reminders'
+  import { HashRouter, Routes, Route } from 'react-router-dom'
+  import Layout from './components/layout/Layout'
+  import Dashboard from './pages/Dashboard'
+  import ServiceOrders from './pages/ServiceOrders'
+  import Catalog from './pages/Catalog'
+  import Reminders from './pages/Reminders'
+  import VehicleList from './pages/vehicles/VehicleList'
+  import VehicleForm from './pages/vehicles/VehicleForm'
+  import VehicleDetail from './pages/vehicles/VehicleDetail'
+  import OwnerList from './pages/owners/OwnerList'
+  import OwnerForm from './pages/owners/OwnerForm'
+  import OwnerDetail from './pages/owners/OwnerDetail'
 
-export default function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="vehicles" element={<Vehicles />} />
-          <Route path="owners" element={<Owners />} />
-          <Route path="service-orders" element={<ServiceOrders />} />
-          <Route path="catalog" element={<Catalog />} />
-          <Route path="reminders" element={<Reminders />} />
-        </Route>
-      </Routes>
-    </HashRouter>
-  )
-}
+  export default function App() {
+    return (
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Dashboard />} />
+
+            <Route path="vehicles" element={<VehicleList />} />
+            <Route path="vehicles/new" element={<VehicleForm />} />
+            <Route path="vehicles/:id" element={<VehicleDetail />} />
+            <Route path="vehicles/:id/edit" element={<VehicleForm />} />
+
+            <Route path="owners" element={<OwnerList />} />
+            <Route path="owners/new" element={<OwnerForm />} />
+            <Route path="owners/:id" element={<OwnerDetail />} />
+            <Route path="owners/:id/edit" element={<OwnerForm />} />
+
+            <Route path="service-orders" element={<ServiceOrders />} />
+            <Route path="catalog" element={<Catalog />} />
+            <Route path="reminders" element={<Reminders />} />
+          </Route>
+        </Routes>
+      </HashRouter>
+    )
+  }
